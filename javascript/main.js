7d31756aa21c28f1f997860c202ae19e1d9d21d7
Ricardo Yepez <ricardoyepezz@gmail.com>
2021-12-26 17:59:33 -0300

    Desafío algoritmo con condicional

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,27 @@
+//--------------- DESAFÍO 2 -----------------//
+
 //Título
-alert("Cálculo de tu edad futura");
+alert("Eres mayor de edad??");
 
 //Variables de entrada tipo string
 let name = prompt("Por favor coloca tu Nombre");
 let lastname = prompt("Por favor coloca tu Apellido");
 
 //Variables de entrada tipo string con su respectivas conversiones a datos númericos
-let ageIn = prompt("Por favor coloca tu edad");
-let ageOut = parseInt(ageIn);
-let yearIn = prompt("Por favor un año para calcular tu edad en el futuro");
-let yearOut = parseInt(yearIn);
+let ageIn = Number(prompt("Por favor coloca el año en el que naciste:"));
 
-//Calculo de edad futura
-let cal = yearOut - 2021 + ageOut;
+//Calculo de edad
+let cal = 2021 - ageIn;
 
 //función if para mostrar cálculo solo si el año de entrada en mayor a 2021, sino mostrará que el dato es incorrecto
-if (yearOut > 2021) {
-	alert(`${name} ${lastname} tendrá ${cal} años para el año ${yearOut}.`);
-} else {
+if (ageIn > 2021) {
 	alert("Año incorrecto");
+
+	//condiciones para validar si la persona es mayor o menor de edad
+} else if (cal >= 18) {
+	alert(`${name} ${lastname} tiene ${cal} años, es mayor de edad.`);
+} else if (cal < 18) {
+	alert(`${name} ${lastname} tiene ${cal} años, es menor de edad.`);
+} else {
+	alert("Datos incorrectos");
 }

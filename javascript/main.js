3a964dbe53b769b906e73ccecf45a6adabf85eea
Ricardo Yepez <ricardoyepezz@gmail.com>
2022-01-07 19:47:49 -0300

    Desafío Complementario

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,27 @@
-/* //--------------- DESAFÍO 3 Ciclos/iteraciones -----------------// */
+//--------------- DESAFÍO COMPLEMENTARIO -----------------//
 
-alert("Escoge una opción:");
-let option = Number(
-	prompt(
-		" 1. Últimos pilotos de F1 campeones del mundo - 2. Últimos equipos de F1 campeones del mundo"
-	)
-);
+alert("Cálculo de Área:");
+let option = Number(prompt(" 1. Triángulo - 2. Cuadrado - 3. Círculo"));
 
+// ---------------triangle calculation---------------//
 if (option === 1) {
-	const cars = [
-		"Max Verstappen",
-		"Lewis Hamilton",
-		"Nico Rosberg",
-		"Sebastian Vettel",
-	];
-	let text = "";
-	for (let i = 0; i < cars.length; i++) {
-		text = cars[i] + " ";
-		console.log(text);
-	}
+	let firstValue = Number(prompt("Ingrese la base en metros"));
+	let secondValue = Number(prompt("Ingrese la altura en metros"));
+	const triangle = (a, b) => ((a * b) / 2).toFixed(2);
+	alert(`${triangle(firstValue, secondValue)} metros cuadrados.`);
+
+	// ---------------square calculation---------------//
 } else if (option === 2) {
-	const cars = ["Mercedes ", "Red Bull", "Brawn", "Ferrari"];
-	let text = "";
-	for (let i = 0; i < cars.length; i++) {
-		text = cars[i] + " ";
-		console.log(text);
-	}
+	let firstValue = Number(prompt("Ingrese la longitud de los lados en metros"));
+	const square = (a) => (a * a).toFixed(2);
+	alert(`${square(firstValue)} metros cuadrados.`);
+
+	// ---------------circle calculation---------------//
+} else if (option === 3) {
+	let firstValue = Number(prompt("Ingrese el radio en metros"));
+	const circle = (a) => (Math.PI * a ** 2).toFixed(2);
+	alert(`${circle(firstValue)} metros cuadrados.`);
+	// --------------- incorrect value ---------------//
 } else {
 	alert("Opción Incorrecta");
 }
